49179ffa7dce14e146a031f335debc5dccdaef1b
Vinson Chuong <vinsonchuong@gmail.com>
2016-08-15 13:19:06 -0700

    Initial implementation

---
+++ spec/index_spec.js
@@ -0,0 +1,34 @@
+import register from 'test-inject';
+
+describe('test-inject', () => {
+  it('can inject dependencies into a test case and tear them down afterwards', () => {
+    const fooSpy = jasmine.createSpy('foo');
+    const barSpy = jasmine.createSpy('bar');
+    const testSpy = jasmine.createSpy('test');
+
+    const inject = register({
+      foo: {
+        setUp: () => fooSpy,
+        tearDown: (foo) => foo()
+      },
+      bar: {
+        setUp: () => barSpy,
+        tearDown: (bar) => bar()
+      }
+    });
+
+    const wrappedTest = inject((foo, bar) => {
+      testSpy(foo, bar);
+    });
+
+    expect(fooSpy).not.toHaveBeenCalled();
+    expect(barSpy).not.toHaveBeenCalled();
+    expect(testSpy).not.toHaveBeenCalled();
+
+    wrappedTest();
+
+    expect(fooSpy).toHaveBeenCalled();
+    expect(barSpy).toHaveBeenCalled();
+    expect(testSpy).toHaveBeenCalledWith(fooSpy, barSpy);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-function count(total) {
-  return new Promise((resolve) => {
-    function loop(current) {
-      if (current > 0) {
-        process.stdout.write(`${current}...`);
-        setTimeout(() => loop(current - 1), 1000);
-      } else {
-        resolve();
-      }
-    }
-    loop(total);
-  });
+function parameterNames(fn) {
+  const [, names] = fn.toString().match(/^.*?\((.*?)\)/);
+  return names.split(/\s*,\s*/).filter(Boolean);
 }
 
-export default async function() {
-  await count(3);
-  process.stdout.write('Hello World!\n');
+export default function(dependencies) {
+  return (test) => {
+    const testDependencies = parameterNames(test)
+      .map((name) => {
+        const {setUp, tearDown} = dependencies[name];
+        const value = setUp();
+        return {
+          value,
+          tearDown: () => tearDown(value)
+        };
+      });
+
+    return () => {
+      try {
+        test(...testDependencies.map(({value}) => value));
+      } finally {
+        testDependencies.forEach(({tearDown}) => {
+          tearDown();
+        });
+      }
+    };
+  };
 }
